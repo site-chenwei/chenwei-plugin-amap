2c020591c8902e38153d2cc27816a6caac1f7b37
ChenWei <1458010118@qq.com>
2019-09-05 14:29:55 +0800

    暂时移除地理围栏功能。完善返回信息

--- src/main/AMapPlugin.java
+++ src/main/AMapPlugin.java
@@ -6,8 +6,6 @@ import android.content.Context;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 
-import com.amap.api.fence.GeoFence;
-import com.amap.api.fence.GeoFenceClient;
 import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.CoordinateConverter;
@@ -62,22 +60,7 @@ public class AMapPlugin extends CordovaPlugin {
             result.setKeepCallback(true);
             callbackContext.sendPluginResult(result);
             getWeatherInfo(callbackContext, args);
-        } else if (MethodNames.GEO_FENCE_IN.equalsIgnoreCase(action)) {
-            PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
-            result.setKeepCallback(true);
-            callbackContext.sendPluginResult(result);
-            geoFenceIn(callbackContext, args);
-        } else if (MethodNames.GEO_FENCE_OUT.equalsIgnoreCase(action)) {
-            PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
-            result.setKeepCallback(true);
-            callbackContext.sendPluginResult(result);
-            geoFenceOut(callbackContext, args);
-        } else if (MethodNames.GEO_FENCE_STAYED.equalsIgnoreCase(action)) {
-            PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
-            result.setKeepCallback(true);
-            callbackContext.sendPluginResult(result);
-            geoFenceStayed(callbackContext, args);
-        } else {
+        }  else {
             LOG.i(TAG, "The Action" + action + "Not Exist");
             PluginResult result = new PluginResult(PluginResult.Status.INVALID_ACTION, "方法调用失败，不存在该方法");
             result.setKeepCallback(false);
@@ -90,17 +73,6 @@ public class AMapPlugin extends CordovaPlugin {
         String GET_LOCATION = "getLocation";
         String GET_WEATHER_INFO = "getWeatherInfo";
         String CALCULATE_DISTANCE = "calculateDistance";
-        String GEO_FENCE_IN = "geoFenceIn";
-        String GEO_FENCE_OUT = "geoFenceOut";
-        String GEO_FENCE_STAYED = "geoFenceStayed";
-    }
-
-    public interface GeoFenceCallBackType {
-        String ADD_SUCCESS = "ADD_SUCCESS";
-        String ADD_ERROR = "ADD_ERROR";
-        String GEO_FENCE_IN = "GEO_FENCE_IN";
-        String GEO_FENCE_OUT = "GEO_FENCE_OUT";
-        String GEO_FENCE_STAYED = "GEO_FENCE_STAYED";
     }
 
     private void getLocation(CallbackContext callbackContext, JSONArray args) {
@@ -134,52 +106,6 @@ public class AMapPlugin extends CordovaPlugin {
             endPoint.setLongitude(args.getLong(3));
             float v = CoordinateConverter.calculateLineDistance(startPoint, endPoint);
             callbackContext.success(String.valueOf(v));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void geoFenceIn(CallbackContext callbackContext, JSONArray args) {
-        if (geoFenceInClient != null) {
-            geoFenceInClient.removeGeoFence();
-        } else {
-            geoFenceInClient = new GeoFenceClient(context);
-        }
-        geoFenceInClient.setActivateAction(GeoFenceClient.GEOFENCE_IN);
-        createGeoFence(callbackContext, args, geoFenceInClient);
-    }
-
-    private void geoFenceOut(CallbackContext callbackContext, JSONArray args) {
-        if (geoFenceOutClient != null) {
-            geoFenceOutClient.removeGeoFence();
-        } else {
-            geoFenceOutClient = new GeoFenceClient(context);
-        }
-        geoFenceOutClient.setActivateAction(GeoFenceClient.GEOFENCE_OUT);
-        createGeoFence(callbackContext, args, geoFenceOutClient);
-    }
-
-    private void geoFenceStayed(CallbackContext callbackContext, JSONArray args) {
-        if (geoFenceStayedClient != null) {
-            geoFenceStayedClient.removeGeoFence();
-        } else {
-            geoFenceStayedClient = new GeoFenceClient(context);
-        }
-        geoFenceStayedClient.setActivateAction(GeoFenceClient.GEOFENCE_STAYED);
-        createGeoFence(callbackContext, args, geoFenceStayedClient);
-    }
-
-    private void createGeoFence(CallbackContext callbackContext, JSONArray args, GeoFenceClient geoFenceClient) {
-        DPoint centerPoint = new DPoint();
-        try {
-            centerPoint.setLatitude(args.getDouble(0));
-            centerPoint.setLongitude(args.getDouble(1));
-            geoFenceClient.addGeoFence(centerPoint, args.getInt(0), "");
-            geoFenceClient.setGeoFenceListener(new AMapGenFenceListener(callbackContext));
-            geoFenceClient.createPendingIntent(GEO_FENCE_BROADCAST_ACTION);
-            IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
-            filter.addAction(GEO_FENCE_BROADCAST_ACTION);
-            context.registerReceiver(new AMapGeoFenceReceiver(callbackContext), filter);
         } catch (JSONException e) {
             e.printStackTrace();
         }
--- src/main/AMapPlugin.js
+++ src/main/AMapPlugin.js
@@ -1,22 +1,14 @@
 function AMapPlugin() {
 
 }
+
 AMapPlugin.prototype.getLocation = function (success, fail) {
     cordova.exec(success, fail, 'AMapPlugin', 'getLocation', []);
 }
 AMapPlugin.prototype.getWeatherInfo = function (params, success, fail) {
-    cordova.exec(success, fail, 'AMapPlugin', 'getWeatherInfo', [params]);
+    cordova.exec(success, fail, 'AMapPlugin', 'getWeatherInfo', [params.adCode]);
 }
 AMapPlugin.prototype.calculateDistance = function (params, success, fail) {
     cordova.exec(success, fail, 'AMapPlugin', 'calculateDistance', [params.startLatitude, params.startLongitude, params.endLatitude, params.endLongitude]);
-}
-AMapPlugin.prototype.geoFenceIn = function (params, success, fail) {
-    cordova.exec(success, fail, 'AMapPlugin', 'geoFenceIn', [params]);
-}
-AMapPlugin.prototype.geoFenceOut = function (params, success, fail) {
-    cordova.exec(success, fail, 'AMapPlugin', 'geoFenceOut', [params]);
-}
-AMapPlugin.prototype.geoFenceStayed = function (params, success, fail) {
-    cordova.exec(success, fail, 'AMapPlugin', 'geoFenceStayed', [params]);
 }
 module.exports = new AMapPlugin();

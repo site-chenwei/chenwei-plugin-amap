9f3b764ffd34eec949b772cea213dd2feb502ac5
ChenWei <1458010118@qq.com>
2019-09-05 12:47:58 +0800

    拆分插件源码与指引文件

--- src/main/AMapPlugin.js
+++ src/main/AMapPlugin.js
@@ -1,25 +1,22 @@
-var exec = require('cordova.exec');
-
 function AMapPlugin() {
 
 }
-
 AMapPlugin.prototype.getLocation = function (success, fail) {
-    exec(success, fail, 'AMapPlugin', 'getLocation', []);
+    cordova.exec(success, fail, 'AMapPlugin', 'getLocation', []);
 }
 AMapPlugin.prototype.getWeatherInfo = function (params, success, fail) {
-    exec(success, fail, 'AMapPlugin', 'getWeatherInfo', [params]);
+    cordova.exec(success, fail, 'AMapPlugin', 'getWeatherInfo', [params]);
 }
 AMapPlugin.prototype.calculateDistance = function (params, success, fail) {
-    exec(success, fail, 'AMapPlugin', 'calculateDistance', [params.startLatitude, params.startLongitude, params.endLatitude, params.endLongitude]);
+    cordova.exec(success, fail, 'AMapPlugin', 'calculateDistance', [params.startLatitude, params.startLongitude, params.endLatitude, params.endLongitude]);
 }
 AMapPlugin.prototype.geoFenceIn = function (params, success, fail) {
-    exec(success, fail, 'AMapPlugin', 'geoFenceIn', [params]);
+    cordova.exec(success, fail, 'AMapPlugin', 'geoFenceIn', [params]);
 }
 AMapPlugin.prototype.geoFenceOut = function (params, success, fail) {
-    exec(success, fail, 'AMapPlugin', 'geoFenceOut', [params]);
+    cordova.exec(success, fail, 'AMapPlugin', 'geoFenceOut', [params]);
 }
 AMapPlugin.prototype.geoFenceStayed = function (params, success, fail) {
-    exec(success, fail, 'AMapPlugin', 'geoFenceStayed', [params]);
+    cordova.exec(success, fail, 'AMapPlugin', 'geoFenceStayed', [params]);
 }
 module.exports = new AMapPlugin();

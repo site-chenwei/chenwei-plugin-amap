07d54edc42eff4a05f2122c88fb44f3bbf933967
ChenWei <1458010118@qq.com>
2019-09-05 11:59:49 +0800

    移除无用引用，完善插件配置

--- src/main/AMapPlugin.js
+++ src/main/AMapPlugin.js
@@ -1,23 +1,25 @@
 var exec = require('cordova.exec');
 
-var AMapPlugin = {
-    getLocation: (success, fail) => {
-        exec(success, fail, 'AMapPlugin', 'getLocation', []);
-    },
-    getWeatherInfo: (params, success, fail) => {
-        exec(success, fail, 'AMapPlugin', 'getWeatherInfo', [params]);
-    },
-    calculateDistance: (params, success, fail) => {
-        exec(success, fail, 'AMapPlugin', 'calculateDistance', [params.startLatitude,params.startLongitude,params.endLatitude,params.endLongitude]);
-    },
-    geoFenceIn: (params, success, fail) => {
-        exec(success, fail, 'AMapPlugin', 'geoFenceIn', [params]);
-    },
-    geoFenceOut: (params, success, fail) => {
-        exec(success, fail, 'AMapPlugin', 'geoFenceOut', [params]);
-    },
-    geoFenceStayed: (params, success, fail) => {
-        exec(success, fail, 'AMapPlugin', 'geoFenceStayed', [params]);
-    }
-};
-module.exports = AMapPlugin;
+function AMapPlugin() {
+
+}
+
+AMapPlugin.prototype.getLocation = function (success, fail) {
+    exec(success, fail, 'AMapPlugin', 'getLocation', []);
+}
+AMapPlugin.prototype.getWeatherInfo = function (params, success, fail) {
+    exec(success, fail, 'AMapPlugin', 'getWeatherInfo', [params]);
+}
+AMapPlugin.prototype.calculateDistance = function (params, success, fail) {
+    exec(success, fail, 'AMapPlugin', 'calculateDistance', [params.startLatitude, params.startLongitude, params.endLatitude, params.endLongitude]);
+}
+AMapPlugin.prototype.geoFenceIn = function (params, success, fail) {
+    exec(success, fail, 'AMapPlugin', 'geoFenceIn', [params]);
+}
+AMapPlugin.prototype.geoFenceOut = function (params, success, fail) {
+    exec(success, fail, 'AMapPlugin', 'geoFenceOut', [params]);
+}
+AMapPlugin.prototype.geoFenceStayed = function (params, success, fail) {
+    exec(success, fail, 'AMapPlugin', 'geoFenceStayed', [params]);
+}
+module.exports = new AMapPlugin();
